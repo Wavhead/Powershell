cf1b6d6b5ac2cc4b87996237b420541e41b1e00c
Wavhead <111568462+Wavhead@users.noreply.github.com>
2022-10-28 00:56:11 -0700

    Add files via upload

---
+++ runThis.ps1
@@ -0,0 +1,10 @@
+﻿for ($i=1; $i -le 99; $i++) {   # Assign $i to 1 and increment each step, (-le) less than 100.
+    $i                           # Print $i.
+    if ($i -le 50) {             # While $i less than 50, print half empty.
+        'half empty'
+    } else {                     # Else print half full.
+        'half full'
+    }
+}
+$i                               # Print final value.
+'full'                           # Print full When done.
